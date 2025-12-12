7c81e5c5ef01b1c023dca0670e877d0243c67c0d
Matheus Nunes <matheusjobs04@gmail.com>
2025-12-12 19:30:00 -0300

    Estrutura

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+      <div className="border-orange-600 border-2"> Área topo </div>
+      <div className="border-green-600 border-2"> Área conteúdos </div>
+      <div className="border-purple-600 border-2"> Rodapé </div>
+    </>
+  );
+}
